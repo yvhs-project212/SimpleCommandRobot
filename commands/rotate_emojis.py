4f08bbf9613c0355f13e2f1b0c11f12ac7496e84
Jon Solera <jon@emufarm.org>
2024-01-25 13:59:42 -0800

    fix a couple of bugs, add a lot of documentation

--- commands/rotate_emojis.py
+++ commands/rotate_emojis.py
@@ -15,36 +15,92 @@ import constants
 
 
 class RotateEmojisLeft(commands2.Command):
+    """
+    This imaginary robot has an "emoji subsystem" that displays different
+    emojis.  A motor is used to rotate the display left (showing the previous
+    emoji) or right (showing the next emoji).  Running the motor for exactly
+    2.0 seconds shifts the display by one emoji.
+    """
     def __init__(self, emoji_ss: EmojiSubsystem) -> None:
+        """
+        Constructor for the command object.  Assigns some instance variables.
+        """
         super().__init__()
         self.emoji_ss = emoji_ss
         self.timer = wpilib.Timer()
+
+    def initialize(self):
+        """
+        Perform any setup to initialize the command.
+        This method runs when the scheduler schedules the command.
+        """
         self.timer.start()
 
     def execute(self):
+        """
+        Performs the main bulk of the command.
+        This method runs 50 times a second while the command is active.
+        """
         self.emoji_ss.rotate_left()
 
     def isFinished(self):
+        """
+        Returns a boolean indicating whether the command has completed.
+        """
         # stop the motor after 2 seconds
         return self.timer.get() >= 2.0
 
-    def cancel(self):
+    def end(self, interrupted: bool):
+        """
+        Perform any cleanup or final steps needed after the command finishes.
+        If you need to do something different depending on whether the command
+        completed normally or was interrupted (by another command), the
+        :interrupted: parameter will be True if the command was interrupted.
+        """
         self.emoji_ss.stop()
 
 
 class RotateEmojisRight(commands2.Command):
+    """
+    This imaginary robot has an "emoji subsystem" that displays different
+    emojis.  A motor is used to rotate the display left (showing the previous
+    emoji) or right (showing the next emoji).  Running the motor for exactly
+    2.0 seconds shifts the display by one emoji.
+    """
     def __init__(self, emoji_ss: EmojiSubsystem) -> None:
+        """
+        Constructor for the command object.  Assigns some instance variables.
+        """
         super().__init__()
         self.emoji_ss = emoji_ss
         self.timer = wpilib.Timer()
+
+    def initialize(self):
+        """
+        Perform any setup to initialize the command.
+        This method runs when the scheduler schedules the command.
+        """
         self.timer.start()
 
     def execute(self):
+        """
+        Performs the main bulk of the command.
+        This method runs 50 times a second while the command is active.
+        """
         self.emoji_ss.rotate_right()
 
     def isFinished(self):
+        """
+        Returns a boolean indicating whether the command has completed.
+        """
         # stop the motor after 2 seconds
         return self.timer.get() >= 2.0
 
-    def cancel(self):
+    def end(self, interrupted: bool):
+        """
+        Perform any cleanup or final steps needed after the command finishes.
+        If you need to do something different depending on whether the command
+        completed normally or was interrupted (by another command), the
+        :interrupted: parameter will be True if the command was interrupted.
+        """
         self.emoji_ss.stop()
--- subsystems/emoji_ss.py
+++ subsystems/emoji_ss.py
@@ -1,9 +1,3 @@
-#
-# Copyright (c) FIRST and other WPILib contributors.
-# Open Source Software; you can modify and/or share it under the terms of
-# the WPILib BSD license file in the root directory of this project.
-#
-
 import wpilib
 import commands2
 import rev
@@ -11,6 +5,20 @@ from constants import ELEC
 
 
 class EmojiSubsystem(commands2.Subsystem):
+    """
+    This imaginary robot has an "emoji subsystem" that displays different
+    emojis.  A motor is used to rotate the display left (showing the previous
+    emoji) or right (showing the next emoji).
+
+    This is intended as a very simple idea that resembles several real
+    subsystems that use a single motor in forward or reverse.
+
+    When writing code for a command-based robot, first create the subsystems
+    (before writing the commands).  Subsystem methods should be provided to
+    read any sensors, and to activate/deactivate any actuators.  Use the
+    constants.py file to specify values such as motor CAN bus IDs or which
+    RoboRIO port a sensor cable is plugged into.
+    """
     def __init__(self) -> None:
         """Creates a new EmojiSubsystem"""
         super().__init__()
@@ -18,10 +26,22 @@ class EmojiSubsystem(commands2.Subsystem):
             ELEC.emoji_motor_CAN_ID, rev.CANSparkMax.MotorType.kBrushless)
 
     def rotate_left(self):
+        """
+        An example method that activates a subsystem actuator -- in this case,
+        turning the motor left.
+        """
         self.motor.set(-0.3)
 
     def rotate_right(self):
+        """
+        An example method that activates a subsystem actuator -- in this case,
+        turning the motor right.
+        """
         self.motor.set(0.3)
 
     def stop(self):
+        """
+        An example method that deactivates a subsystem actuator -- in this case,
+        turning the motor off.
+        """
         self.motor.set(0)
